9145f05dedeea1129e1d899de451670e2824b806
sarai <saraicarballosanchez@gmail.com>
2025-02-18 15:21:34 +0100

    Actividad 5 ejercicio 3 real

--- Actividad 5/exercise3/start/Astronomy/Astronomy/AppShell.xaml.cs
+++ Actividad 5/exercise3/start/Astronomy/Astronomy/AppShell.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using Astronomy.Pages;
 
 namespace Astronomy;
-
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
     }
 }
--- Actividad 5/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
+++ Actividad 5/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -2,9 +2,13 @@ namespace Astronomy.Pages;
 
 public partial class AstronomicalBodiesPage : ContentPage
 {
-	public AstronomicalBodiesPage()
-	{
-		InitializeComponent();
+    public AstronomicalBodiesPage()
+    {
+        InitializeComponent();
 
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
     }
 }
