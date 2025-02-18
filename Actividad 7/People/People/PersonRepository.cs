b08f6e120f514fc82868b7b5f4f56deb88183a49
sarai <saraicarballosanchez@gmail.com>
2025-02-18 16:32:43 +0100

    Actividad 7, EJERCICIO 1

---
+++ Actividad 7/People/People/PersonRepository.cs
@@ -0,0 +1,63 @@
+﻿using SQLite;
+using People.Models;
+using System;
+using System.Collections.Generic;
+
+namespace People;
+
+public class PersonRepository
+{
+    private SQLiteConnection conn;
+    private string _dbPath;
+    public string StatusMessage { get; set; }
+
+    public PersonRepository(string dbPath)
+    {
+        _dbPath = dbPath;
+        Init();
+    }
+
+    private void Init()
+    {
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
+    }
+
+    public void AddNewPerson(string name)
+    {
+        int result = 0;
+        try
+        {
+            Init();
+
+            if (string.IsNullOrEmpty(name))
+                throw new Exception("Valid name required");
+
+            result = conn.Insert(new Person { Name = name });
+
+            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+        }
+    }
+
+    public List<Person> GetAllPeople()
+    {
+        try
+        {
+            Init();
+            return conn.Table<Person>().ToList();
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+        }
+
+        return new List<Person>();
+    }
+}
