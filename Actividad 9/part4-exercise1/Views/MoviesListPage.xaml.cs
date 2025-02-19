a7c1c43ceb3d553b79c903d8e6316d633bf03c01
sarai <saraicarballosanchez@gmail.com>
2025-02-19 01:08:14 +0100

    ACTIVIDAD 9 EJERCICIO 1

--- Actividad 9/part4-exercise1/Views/MoviesListPage.xaml.cs
+++ Actividad 9/part4-exercise1/Views/MoviesListPage.xaml.cs
@@ -1,16 +1,17 @@
+
+
 namespace MovieCatalog.Views;
 
 public partial class MoviesListPage : ContentPage
 {
-	public MoviesListPage()
-	{
-		InitializeComponent();
-	}
+    public MoviesListPage()
+    {
+        InitializeComponent();
+    }
 
     private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
     {
-		ViewModels.MovieViewModel movie = (ViewModels.MovieViewModel)e.Item;
-		await Navigation.PushAsync(new Views.MovieDetailPage(movie));
+        await Navigation.PushAsync(new MovieDetailPage());
     }
 
     private void MenuItem_Clicked(object sender, EventArgs e)
